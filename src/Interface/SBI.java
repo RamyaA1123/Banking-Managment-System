535260aef9a1d667d442d8fdd2dd1b41e9a4b28a
RamyaA1123 <ramyakplanand@gmail.com>
2026-04-23 23:59:43 +0530

    Add files via upload

---
+++ src/Interface/SBI.java
@@ -0,0 +1,9 @@
+package Interface;
+//Using Interface Before java 1.8
+public interface SBI {
+	void deposite(double amount);
+    void withdrawl(double amount);
+    double checkBalance();
+    void accountDetails();
+
+}
---
+++ src/UserDefinedException/InvalidAmount.java
@@ -0,0 +1,8 @@
+package UserDefinedException;
+//checked
+public class InvalidAmount extends Exception{
+	public InvalidAmount(String msg) {
+		  System.out.println(msg);
+	    }
+
+}
