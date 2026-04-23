4de48dc4f35adb49e14d067103b9352d29adab7f
RamyaA1123 <ramyakplanand@gmail.com>
2026-04-23 23:42:56 +0530

    Delete BankingManagmentSystem/src/Interface directory

--- BankingManagmentSystem/src/Interface/SBI.java
+++
@@ -1,9 +0,0 @@
-package Interface;
-//Using Interface Before java 1.8
-public interface SBI {
-	void deposite(double amount);
-    void withdrawl(double amount);
-    double checkBalance();
-    void accountDetails();
-
-}
